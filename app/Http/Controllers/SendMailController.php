e36f3b46eef741aecc38d574b45df096e0aa7b24
charles <jAyaT3rus>
2022-07-15 15:45:03 +0200

    T5. Implement queue and adjusted new html parameter.

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\CustomMailer\CustomTransport;
+use App\Jobs\ProcessEmails;
 use Illuminate\Support\Facades\Config;
 
 class SendMailController extends Controller
@@ -36,11 +37,13 @@ class SendMailController extends Controller
             }            
         }
         
+        // Process from, message and subject data. 
         $transport->setFrom($request->get('from_email'), ($request->get('from_name') !== null ? $request->get('from_name') : ''));
         $transport->setMessage($request->get('message'));
         $transport->setSubject($request->get('subject'));
         
-        if ($request->get('html') == true) {
+        // Check if user can set html. 
+        if ($request->get('mail_type') == 'html') {
             $transport->setHtml();
         }
 
@@ -51,8 +54,16 @@ class SendMailController extends Controller
             $extra_parameters['CustomID'] = $request->get('custom_id');
         }
 
-        $response = $transport->send($request->get('mail_type'));
-
-        echo json_encode($response);
+        // Check whether task will be queued. 
+        $queue = $request->get('queue');
+        if ($queue == 'true') {
+            $transportJobs = new ProcessEmails($transport, $request->get('driver_type'));
+            $this->dispatch($transportJobs);
+        }
+        else {
+            $response = $transport->processSend($request->get('driver_type'));  
+            echo json_encode(array('success' =>$response));
+        }
+        
     }
 }
